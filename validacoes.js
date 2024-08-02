40122aa02ffa63de066d512f64afb697f60fab9d
Carlos Piroca <90231560+calang000@users.noreply.github.com>
2024-08-02 18:27:03 -0300

    Update validacoes.js

--- validacoes.js
+++ validacoes.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
   res.status(200).json({
-    machine_ids_validos: ["9e94383b41394f65909b625ff38183ba1508b5ccfa0e426b544f4945531278ac", "ID654321", "ID789012"]
+    machine_ids_validos: ["9e94383b41394f65909b625ff38183ba1508b5ccfa0e426b544f4945531278ac", "ID9e94383b41394f65909b625ff38183ba1508b5ccfa0e426b544f4945531278ac", "ID789012"]
   });
 }
